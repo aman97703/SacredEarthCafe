f5fcd9f2f80cb643c4c904dbfc45bbedf70cc283
Aman Gupta <68396428+aman97703@users.noreply.github.com>
2023-04-20 00:49:54 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
+import './Assets/Css/Common.css';
+import './App.css'
+import { Provider } from "react-redux";
+import { store } from "./store";
+import Cart from "./pages/Cart";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Provider store={store}>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
+      </Provider>
+    </Router>
   );
-}
+};
 
 export default App;
